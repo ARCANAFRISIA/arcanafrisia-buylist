4287b2063218b252e9734d1aa36e3427fbe8e481
Hindrik Arendz <info@arcanafrisia.com>
2025-10-16 11:23:01 +0200

    chore: redirect / to /buylist

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,14 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/', destination: '/buylist', permanent: true },
+    ];
+  },
+};
+
+export default nextConfig;
